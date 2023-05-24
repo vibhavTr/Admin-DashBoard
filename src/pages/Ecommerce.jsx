3ee0c56e0289d7773f700a1b62b52047ab49719d
Vibhav Trivedi <vibhavtrivedi15@gmail.com>
2023-05-24 16:50:30 +0530

    revenue updates

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "../components";
 import { earningData } from "../data/dummy";
+import { GoPrimitiveDot } from 'react-icons/go';
 
 const Ecommerce = () => {
   return (
@@ -36,6 +37,39 @@ const Ecommerce = () => {
               <p className="text-sm text-gray-400 mt-1">{item.title}</p>
             </div>
           ))}
+        </div>
+        <div className="flex gap-10 flex-wrap justify-center">
+          <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+            <div className="flex justify-between">
+              <p className="font-semibold text-xl">
+                Revenue Updates
+              </p>
+              <div className="flex items-center gap-4">
+                <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                  <span><GoPrimitiveDot /></span>
+                  <span>Expense</span>
+                </p>
+                <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                  <span><GoPrimitiveDot /></span>
+                  <span>Budget</span>
+                </p>
+              </div>
+            </div>
+            <div className="mt-10 flex gap-10 flex-wrap justify-center">
+              <div className="border-r-1 border-color m-4 pr-10">
+                <div>
+                  <p>
+                    <span className="text-3xl font-semibold">
+                      $93,438
+                    </span>
+                    <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-right bg-green-400 ml-3 text-xs">
+                      23%
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
     </div>
   );
